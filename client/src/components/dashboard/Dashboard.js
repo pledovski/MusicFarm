b1b8e607b025d881e645ab175712dd2b4a5d3f39
pledovski <human.pled@gmail.com>
2019-06-30 18:52:50 +0300

    Better Berch

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,9 +3,16 @@ import React from 'react';
 // import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
+  var dashStyle = {
+    color: 'red',
+    fontSize: 60,
+    display: 'flex',
+    justifyContent: 'space-around',
+  }
+
   return (
     <div>
-      <h1>The service is under construction!</h1>
+      <h1 style={dashStyle} ><u>The service is under construction!</u></h1>
       <div className="dashboard"></div>
     </div>
   )
