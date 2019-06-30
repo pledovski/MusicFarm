184f56bb6b076fa2bba66ca71f97e97ff9033999
pledovski <human.pled@gmail.com>
2019-06-30 17:46:38 +0300

    Logout implemented

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,15 @@
 import axios from 'axios'
 import { setAlert } from './alert'
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL } from './types'
+import { 
+  REGISTER_SUCCESS, 
+  REGISTER_FAIL,
+  USER_LOADED, 
+  AUTH_ERROR, 
+  LOGIN_SUCCESS, 
+  LOGIN_FAIL,
+  LOGOUT
+} from './types'
+
 import setAuthToken from '../utils/setAuthToken'
 // import { isRegExp } from 'util';
 
@@ -85,4 +94,9 @@ export const login = (email, password) => async dispatch => {
       type: LOGIN_FAIL
     });
   }
+};
+
+//Logout / Clear profile
+export const logout = () => dispatch => {
+  dispatch({ type: LOGOUT });
 }
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
 
-const Navbar = () => {
-  return (
-<nav className="navbar bg-dark">
-      <h1>
-        <Link to="/"><i className="fab fa-earlybirds"></i> MusicFarm</Link>
-      </h1>
-      <ul>
-        <li><Link to="!#">Artists</Link></li>
-        <li><Link to="!#">Records</Link></li>
-        <li><Link to="!#">Labels</Link></li>
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authLinks = (
+    <ul>
+        <li><Link to="#!">Artists</Link></li>
+        <li><Link to="#!">Records</Link></li>
+        <li><Link to="#!">Labels</Link></li>
+        <li><Link onClick={logout} to="#!">
+          <i className="fas fa-sign-out-alt"></i>{' '}
+          <span className="hide-sm">Logout</span></Link>
+        </li>
+      </ul>
+  );
+
+  const guestLinks = (
+    <ul>
+        <li><Link to="#!">Artists</Link></li>
+        <li><Link to="#!">Records</Link></li>
+        <li><Link to="#!">Labels</Link></li>
         <li><Link to="/register" className="btn btn-primary">Sign Up</Link></li>
         <li><Link to="/login">Login</Link></li>
       </ul>
+  );
+
+  return (
+    <nav className="navbar bg-dark">
+      <h1>
+        <Link to="/"><i className="fab fa-earlybirds"></i> MusicFarm</Link>
+      </h1>
+      { !loading && (<Fragment>{ isAuthenticated ? authLinks : guestLinks }</Fragment>) }
     </nav>
-  )
+  );
+};
+
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
 }
 
-export default Navbar
+const mapStateToProps = state => ({
+  auth: state.auth
+})
+
+export default connect(mapStateToProps, { logout })(Navbar)
