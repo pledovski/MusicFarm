3d0ac404b67f5ab1189441f957729ce981176241
pledovski <human.pled@gmail.com>
2019-06-30 20:33:51 +0300

    Profile added

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,7 +7,8 @@ import {
   AUTH_ERROR, 
   LOGIN_SUCCESS, 
   LOGIN_FAIL,
-  LOGOUT
+  LOGOUT,
+  CLEAR_PROFILE
 } from './types'
 
 import setAuthToken from '../utils/setAuthToken'
@@ -99,4 +100,5 @@ export const login = (email, password) => async dispatch => {
 //Logout / Clear profile
 export const logout = () => dispatch => {
   dispatch({ type: LOGOUT });
+  dispatch({ type: CLEAR_PROFILE });
 }
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,38 +1,68 @@
-import React, { Fragment } from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { logout } from '../../actions/auth';
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { logout } from "../../actions/auth";
 
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
-        <li><Link to="#!">Artists</Link></li>
-        <li><Link to="#!">Records</Link></li>
-        <li><Link to="#!">Labels</Link></li>
-        <li><Link onClick={logout} to="#!">
-          <i className="fas fa-sign-out-alt"></i>{' '}
-          <span className="hide-sm">Logout</span></Link>
-        </li>
-      </ul>
+      <li>
+        <Link to="#!">Artists</Link>
+      </li>
+      <li>
+        <Link to="#!">Records</Link>
+      </li>
+      <li>
+        <Link to="#!">Labels</Link>
+      </li>
+      <li>
+        <Link to="/dashboard">
+          <i className="fas fa-user" />{" "}
+          <span className="hide-sm">My profile</span>
+        </Link>
+      </li>
+      <li>
+        <Link onClick={logout} to="#!">
+          <i className="fas fa-sign-out-alt" />{" "}
+          <span className="hide-sm">Logout</span>
+        </Link>
+      </li>
+    </ul>
   );
 
   const guestLinks = (
     <ul>
-        <li><Link to="#!">Artists</Link></li>
-        <li><Link to="#!">Records</Link></li>
-        <li><Link to="#!">Labels</Link></li>
-        <li><Link to="/register" className="btn btn-primary">Sign Up</Link></li>
-        <li><Link to="/login">Login</Link></li>
-      </ul>
+      <li>
+        <Link to="#!">Artists</Link>
+      </li>
+      <li>
+        <Link to="#!">Records</Link>
+      </li>
+      <li>
+        <Link to="#!">Labels</Link>
+      </li>
+      <li>
+        <Link to="/register" className="btn btn-primary">
+          Sign Up
+        </Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </ul>
   );
 
   return (
     <nav className="navbar bg-dark">
       <h1>
-        <Link to="/"><i className="fab fa-earlybirds"></i> MusicFarm</Link>
+        <Link to="/">
+          <i className="fab fa-earlybirds" /> MusicFarm
+        </Link>
       </h1>
-      { !loading && (<Fragment>{ isAuthenticated ? authLinks : guestLinks }</Fragment>) }
+      {!loading && (
+        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+      )}
     </nav>
   );
 };
@@ -40,10 +70,13 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 Navbar.propTypes = {
   logout: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired
-}
+};
 
 const mapStateToProps = state => ({
   auth: state.auth
-})
+});
 
-export default connect(mapStateToProps, { logout })(Navbar)
+export default connect(
+  mapStateToProps,
+  { logout }
+)(Navbar);
