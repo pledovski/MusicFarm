1f204e7cf87ab0bffd7c6c8c48dcf1ffcbd89047
pledovski <human.pled@gmail.com>
2019-07-02 00:50:41 +0300

    Get profiles (no interface)

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,7 +1,14 @@
 import axios from "axios";
 import { setAlert } from "./alert";
 
-import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE, CLEAR_PROFILE, ACCOUNT_DELETED } from "./types";
+import {
+  GET_PROFILE,
+  GET_PROFILES,
+  PROFILE_ERROR,
+  UPDATE_PROFILE,
+  CLEAR_PROFILE,
+  ACCOUNT_DELETED
+} from "./types";
 
 // Get current profile
 export const getCurrentProfile = () => async dispatch => {
@@ -10,6 +17,42 @@ export const getCurrentProfile = () => async dispatch => {
 
     dispatch({
       type: GET_PROFILE,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Get all profiles
+export const getProfiles = () => async dispatch => {
+  try {
+    const res = await axios.get(`/api/profile/user/${userId}`);
+
+    dispatch({
+      type: GET_PROFILES,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Get profile by ID
+export const getProfileById = userId => async dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
+
+  try {
+    const res = await axios.get("/api/profile");
+
+    dispatch({
+      type: GET_PROFILES,
       payload: res.data
     });
   } catch (err) {
@@ -148,10 +191,10 @@ export const deleteRelease = id => async dispatch => {
 export const deleteAccount = () => async dispatch => {
   if (window.confirm("Are you sure? This can NOT be undone!")) {
     try {
-      const res = await axios.delete('api/profile');
+      const res = await axios.delete("api/profile");
 
       dispatch({ type: CLEAR_PROFILE });
-      dispatch({type: ACCOUNT_DELETED});
+      dispatch({ type: ACCOUNT_DELETED });
 
       dispatch(setAlert("Your account has been deleted"));
     } catch (err) {
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -23,6 +23,12 @@ export default function(state = initialState, action) {
         profile: payload,
         loading: false
       };
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false
+      };
     case PROFILE_ERROR:
       return {
         ...state,
